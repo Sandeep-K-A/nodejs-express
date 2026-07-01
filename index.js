2e3126fa705b308f438b509f0ff8977cf9ad89d7
Sandeep K A <kasandeep42@gmail.com>
2026-07-01 21:29:31 +0530

    feat: server implementation using express

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hello world");
+const express = require("express");
+
+const PORT = 5000;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from HomePage.");
+});
+
+app.get("/about", (req, res) => {
+  res.send("Hello from AboutPage.");
+});
+
+app.get("/contacts", (req, res) => {
+  res.send("Hello from ContactsPage.");
+});
+
+app.listen(PORT, () => {
+  console.log(`server running in port ${PORT}...`);
+});
