d3984bd89a3830583f5889d9e8c1f7ffcf4f6e4b
Sandeep K A <kasandeep42@gmail.com>
2026-07-01 21:52:19 +0530

    feat: new endpoint /users added

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
   res.send("Hello from AboutPage.");
 });
+app.get("/users", (req, res) => {
+  res.json({
+    id: "1",
+    name: "sandeep",
+  });
+});
 
 app.get("/contacts", (req, res) => {
   res.send("Hello from ContactsPage.");
