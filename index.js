45055e3a5d9a27b8abfcecbebee5f2e97924a519
Sandeep K A <kasandeep42@gmail.com>
2026-07-01 21:08:14 +0530

    feat: server implementation using node

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello world");
+const http = require("http");
+const PORT = 5000;
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from server.");
+});
+
+server.listen(PORT, () => {
+  console.log(`server running in port ${PORT}`);
+});
