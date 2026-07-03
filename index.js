09c5c6fb86b8092f53c3e69d9c872302905cc287
Sandeep K A <kasandeep42@gmail.com>
2026-07-03 20:47:44 +0530

    crud starting

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello world");
+import express from "express";
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("hello from server");
+});
+
+app.listen(PORT, () => {
+  console.log(`server running in port ${PORT}`);
+});
